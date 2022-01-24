bb57c0c493185df02c05e313d6c12cc0ca48a69d
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-01-24 14:30:59 +0100

    Add blur filter to the mobile menu

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const hamburgerMenuIcon = document.querySelector('.hamburger_menu_icon');
 const sideNavigationBar = document.querySelector('.mobile_side_nav');
 const menuLinks = document.querySelectorAll('.menu_link');
+const mobileMenu = document.querySelector('.parent_main');
 
 const openMenu = () => {
   sideNavigationBar.classList.add('mobile_side_nav_active');
+  mobileMenu.classList.add('parent_main_blur');
 };
 
 const closeMenu = () => {
   sideNavigationBar.classList.remove('mobile_side_nav_active');
+  mobileMenu.classList.remove('parent_main_blur');
 };
 
 hamburgerMenuIcon.addEventListener('click', openMenu);
